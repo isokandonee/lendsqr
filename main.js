952fc0b2f17193b3941e1394024571e8af43fd73
Ogunniyi Isokan Mitchel <isokandone@gmail.com>
2022-10-14 03:47:36 +0100

    setting up the environment

---
+++ main.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+http.createServer(function (request, response){
+    response.writeHead(200,{
+        'Content-Type': 'text/plain'
+    });
+
+    response.end('Hello World\n');
+}).listen(8082);
+
+// console.log(http.ServerRequest)
